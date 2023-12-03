d6f48085f5b451cfb07a6f752babe1d23182b4cd
vahid <vahid.zahani@gmail.com>
2023-12-03 19:26:16 +0330

    add new class printer setting

--- websocket_reader/Form1.cs
+++ websocket_reader/Form1.cs
@@ -135,14 +135,6 @@ namespace websocket_reader
             HideMainForm(true);//true is hide my form
             label1.Text = "ver : " + Application.ProductVersion;
 
-            setHeaderFooter("footer", "");
-            setHeaderFooter("header", "");
-            setHeaderFooter("margin_bottom", "0.196670");
-            setHeaderFooter("margin_left", "0.196670");
-            setHeaderFooter("margin_right", "0.196670");
-            setHeaderFooter("margin_top", "0.196670");
-            setHeaderFooter("Print_Background", "no");
-            setHeaderFooter("Shrink_To_Fit", "yes");
 
             SetStartup();
 
@@ -230,6 +222,7 @@ namespace websocket_reader
 
                         // خواندن JSON
                         var data = JsonConvert.DeserializeObject<Data_For_Print>(receivedMessage);
+                        var printsetting = JsonConvert.DeserializeObject<print_setting>(data.print_setting);
 
                         if (data.rootPath == null) data.rootPath = "nullnull";
                         if (data.serverAddress == null) data.serverAddress= "nullnull";
@@ -243,8 +236,20 @@ namespace websocket_reader
                         //string printername = "Microsoft Print to PDF";
                         //string printername = receivedMessage.Split('|')[0];
 
-                        string printername = data.printer_name;
-                        SetDefaultPrinter(data.printer_name);
+                        string printername = printsetting.printer_name;
+                        //MessageBox.Show(printsetting.header);
+
+                        
+                        setHeaderFooter("footer", printsetting.footer);
+                        setHeaderFooter("header", printsetting.header);
+                        setHeaderFooter("margin_bottom", printsetting.margin_bottom);
+                        setHeaderFooter("margin_left", printsetting.margin_left);
+                        setHeaderFooter("margin_right", printsetting.margin_right);
+                        setHeaderFooter("margin_top", printsetting.margin_top);
+                        setHeaderFooter("Print_Background", "no");
+                        setHeaderFooter("Shrink_To_Fit", "yes");
+
+                        SetDefaultPrinter(printsetting.printer_name);
 
                         //SetDefaultPageSizeA5(printername);
 
@@ -334,10 +339,23 @@ namespace websocket_reader
         }
     }
 
-
+    public class print_setting
+    {
+        public string id {get ; set; }
+        public string type {get ; set; }
+        public string page_name {get ; set; }
+        public string printer_name {get ; set; }
+        public string margin_top {get ; set; }
+        public string margin_right {get ; set; }
+        public string margin_bottom {get ; set; }
+        public string margin_left {get ; set; }
+        public string header {get ; set; }
+        public string footer {get ; set; }
+        public string page_address { get; set; }
+    }
     public class Data_For_Print
     {
-        public string printer_name { get; set; }
+        public string print_setting { get; set; }
         public string visibleContent { get; set; }
         public string serverAddress { get; set; }
         public string rootPath { get; set; }
